0ad04a26f10cff2daf75695173d58131f3a77fc3
Gustavo Rafael <gustavo-kuhl@hotmail.com>
2022-05-03 23:38:59 -0300

    removido debugger

--- view/pessoa/ViewConsultaPessoa.php
+++ view/pessoa/ViewConsultaPessoa.php
@@ -9,7 +9,6 @@ include __DIR__ . '/../inicio-html.php';
 <script>
     function deletaPessoa($iId) {
         const params = new URLSearchParams();
-        debugger;
         params.append('id', $iId);
         axios.post('/manutencao-pessoa?processo=manutencao&acao=deletar', params);
         document.location.reload(true);
